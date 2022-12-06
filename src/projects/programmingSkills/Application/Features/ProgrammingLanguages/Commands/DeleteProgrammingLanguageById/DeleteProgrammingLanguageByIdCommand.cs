bd8e374ef2c310a3d36a5a88c014d36ad1086a85
Deniz <deniz.dursun1@outlook.com>
2022-12-07 01:39:14 +0300

    .

--- src/projects/programmingSkills/Application/Features/ProgrammingLanguages/Commands/DeleteProgrammingLanguageById/DeleteProgrammingLanguageByIdCommand.cs
+++ src/projects/programmingSkills/Application/Features/ProgrammingLanguages/Commands/DeleteProgrammingLanguageById/DeleteProgrammingLanguageByIdCommand.cs
@@ -1,15 +1,9 @@
-﻿using Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage;
-using Application.Features.ProgrammingLanguages.Dtos;
+﻿using Application.Features.ProgrammingLanguages.Dtos;
 using Application.Features.ProgrammingLanguages.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
 using Domain.Entities;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Features.ProgrammingLanguages.Commands.DeleteProgrammingLanguageById
 {
